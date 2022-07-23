a430fab538701a3a1b219da8f192e837c243cc28
b5050d <bdyer5280@gmail.com>
2022-07-23 15:22:51 -0600

    Continued Updating the journal, its in a good state now

--- 007_journal/journal_gui.py
+++ 007_journal/journal_gui.py
@@ -1,11 +1,6 @@
-from cgitb import text
 import tkinter as tk
 import datetime
 import os
-
-# To Add: Previous Entry Button, Next Entry Button
-# A retrieve button to edit previous Entries
-# Get the current date to show up
 
 currdir=os.path.dirname(__file__)
 logloc=os.path.dirname(os.path.dirname(os.path.dirname(__file__)))+'\\Local_Data'
@@ -27,29 +22,50 @@ dateold_ent.delete('0',tk.END)
 dateold_ent.insert('0',todaydate)
 
 def previous_entry():
-    pass
-    # textbox_old.delete('1.0',tk.END)
-    # dateold=dateold_ent.get()
-    # lastdate=dateold-datetime.timedelta(days=1)
-    # dateold_ent.delete('0',tk.END)
-    # dateold_ent.insert('0',lastdate)
-    # path2old=currdir+'\\'+dateold+'_log.txt'
-    # if os.path.exists(path2old):
-    #     pass
-    # else:
-    #     print('No Previous Entry Exists')
-    #     textbox_new.insert('1.0','No Entry Exists for this date')
-def next_entry():
-    pass
+    date1str=dateold_ent.get()
+    date1=datetime.datetime.strptime(date1str,'%Y-%m-%d')
+    date2=date1-datetime.timedelta(days=1)
+    date2=date2.strftime('%Y-%m-%d')
+    add_entry()
+    dateold_ent.delete('0',tk.END)
+    dateold_ent.insert('0',str(date2))
+    newlogfile=logloc+'\\'+date2+'_Personal_Log.txt'
+    if os.path.exists(newlogfile):
+        textbox_new.delete('1.0',tk.END)
+        with open(newlogfile,"r") as newlog:
+            a=newlog.read()
+        textbox_new.insert('1.0',a)
+    else:
+        textbox_new.delete('1.0',tk.END)
+        textbox_new.insert('1.0',"<No entry exists for this date :/ >")
 
-def del_entry():
-    pass
+def next_entry():
+    date1str=dateold_ent.get()
+    date1=datetime.datetime.strptime(date1str,'%Y-%m-%d')
+    date2=date1+datetime.timedelta(days=1)
+    date2=date2.strftime('%Y-%m-%d')
+    add_entry()
+    dateold_ent.delete('0',tk.END)
+    dateold_ent.insert('0',str(date2))
+    newlogfile=logloc+'\\'+date2+'_Personal_Log.txt'
+    if os.path.exists(newlogfile):
+        textbox_new.delete('1.0',tk.END)
+        with open(newlogfile,"r") as newlog:
+            a=newlog.read()
+        textbox_new.insert('1.0',a)
+    else:
+        textbox_new.delete('1.0',tk.END)
+        textbox_new.insert('1.0',"<No entry exists for this date :/ >")
+
+def clear_entry():
+    textbox_new.delete('1.0',tk.END)
+    textbox_new.insert('1.0',"")
 
 previous_butt=tk.Button(text='Previous Entry',command=previous_entry).grid(row=rv,column=2)
-next_butt=tk.Button(text='Next Entry').grid(row=rv,column=3)
-del_butt=tk.Button(text='Delete Entry').grid(row=rv,column=4)
+next_butt=tk.Button(text='Next Entry',command=next_entry).grid(row=rv,column=3)
+del_butt=tk.Button(text='Delete Entry',command=clear_entry).grid(row=rv,column=4)
 rv+=1
-textbox_new=tk.Text(bg='black',fg='cyan')
+textbox_new=tk.Text()
 textbox_new.grid(row=rv,columnspan=5)
 rv+=1
 datenew_lab=tk.Label(text='Current Date:').grid(row=rv,column=0)
@@ -58,26 +74,54 @@ datenew_ent.grid(row=rv,column=1)
 
 datenew_ent.delete('0',tk.END)
 datenew_ent.insert('0',str(todaydate))
+
 def add_entry():
     text_data=textbox_new.get('1.0',tk.END)
-    date=datenew_ent.get()
+    date=dateold_ent.get()
     newlogfile=logloc+'\\'+date+'_Personal_Log.txt'
     if os.path.exists(newlogfile):
-        print('Log already Exists, just appending for now')
-    else:
         with open(newlogfile,"w") as newtxt:
-            pass
-    print('text_data:',text_data)
-    with open(newlogfile,"a") as newtxt:
-        newtxt.write(text_data)
-    print('File successfully written to :',os.path.basename(newlogfile))
+            newtxt.write(text_data)
+        print('File successfully written to :',os.path.basename(newlogfile))
+    else:
+        if "<No entry exists for this date :/ >" in text_data:
+            print('Skipping, no data entered')
+        else:
+            with open(newlogfile,"w") as newtxt:
+                newtxt.write(text_data)
+            print('File successfully written to :',os.path.basename(newlogfile))
+        
 add_butt=tk.Button(text='Add',command=add_entry).grid(row=rv,column=2)
 rv+=1
 
+#Attempt to load the data from the previous entry
+newlogfile=logloc+'\\'+todaydate+'_Personal_Log.txt'
+if os.path.exists(newlogfile):
 
-# textbox_old=tk.Text(bg='black',fg='orange')
-# # textbox_old.grid(row=rv,columnspan=6)
-# rv+=1
+    with open(newlogfile,"r") as logfile:
+        a=logfile.read()
+        # print(a)
+    textbox_new.delete('1.0',tk.END)
+    textbox_new.insert('1.0',a)
+else:
+    textbox_new.delete('1.0',tk.END)
+    textbox_new.insert('1.0',"<No entry exists for this date :/ >")
 
+from tkinter import messagebox
 
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        text_data=textbox_new.get('1.0',tk.END)
+        date=dateold_ent.get()
+        newlogfile=logloc+'\\'+date+'_Personal_Log.txt'
+        if os.path.exists(newlogfile):
+            pass
+        else:
+            with open(newlogfile,"w") as newtxt:
+                pass
+        with open(newlogfile,"w") as newtxt:
+            newtxt.write(text_data)
+        print('File successfully written to :',os.path.basename(newlogfile))
+        journal_window.destroy()
+journal_window.protocol("WM_DELETE_WINDOW", on_closing)
 journal_window.mainloop()
